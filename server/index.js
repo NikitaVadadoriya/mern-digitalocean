bd971f32cdad822c375102cabf4b2bfc8249f09e
NikitaVadadoriya <nikitavaddoriya0@gmail.com>
2023-08-07 16:14:39 +0530

    1

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const bcrypt = require('bcryptjs')
 const path = require('path')
 app.use(cors())
 app.use(express.json())
+app.use(express.static('static'))
 
 app.post('/api/register', async (req, res) => {
 	console.log(req.body)
