2e117faedcdd3e2041b771bd91ab341842db910c
NikitaVadadoriya <nikitavaddoriya0@gmail.com>
2023-08-09 12:58:01 +0530

    chanes

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
 const db = new Sequelize(
-  { database: 'full-mern-stack-video',
+  { database: 'mernstack',
     username:'root',
     password: 'password',
     port: 3306,
